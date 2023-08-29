7d8df586a3850303b56263057fa851b598519b1e
Akash santhosh <akash777kk@gmail.com>
2023-08-29 21:20:48 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SerializeExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a Student object
+            Student student = new Student { Name = "Alice", Age = 20 };
+
+            // Initialize the BinaryFormatter
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            // Define the file path
+            string filePath = @"C:\TextFolder\serialize.txt";
+
+            // Serialize the Student object
+            using (Stream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            {
+                formatter.Serialize(fileStream, student);
+            }
+
+            Console.WriteLine("Serialization complete.");
+            Console.WriteLine("============================");
+
+            // Deserialize the Student object
+            using (Stream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                Student deserializedStudent = (Student)formatter.Deserialize(fileStream);
+                Console.WriteLine($"Deserialized Student - Name: {deserializedStudent.Name}, Age: {deserializedStudent.Age}");
+            }
+        }
+    
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeExample
+{
+    [Serializable]
+    internal class Student
+    {
+        public int Age { get; set; }
+        public string Name { get; set;}
+
+        public Student() 
+        {
+            Name = "akash";
+            Age = 11;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Age : {Age}  Name : {Name}");
+        }
+    }
+}
